88035d1ed33e3520cff1a2c799ac78428bfcf6fe
Travis CI <stephane.langlois@scopyleft.fr>
2017-11-28 11:19:39 +0100

    Remove stupid promise

--- src/api.js
+++ src/api.js
@@ -7,8 +7,5 @@ const api = {}
  * @return {Promise} json Json file with html and metas.
  */
 api.fetch = apiUrl =>
-  new Promise(resolve =>
-    window.fetch(`https://api.daktary.com/${apiUrl}`)
-      .then(response => response.json())
-      .then(json => resolve(json))
-  )
+  window.fetch(`https://api.daktary.com/${apiUrl}`)
+    .then(response => response.json())
